dd5aa4b7f572943f6e6731ffc43ab49f2bff1462
Squikle <mmishaa1702@gmail.com>
2024-03-10 00:46:04 +0100

    long card content click focuses on input

--- src/components/LongCard/LongCardSide.jsx
+++ src/components/LongCard/LongCardSide.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
 import styles from "./LongCardSide.module.css";
-import { useId } from "react";
+import { useId, useRef } from "react";
 
 export function LongCardSide({ text, image, isEditing, editTextRef }) {
   const contentId = useId();
+  const editingRef = useRef();
 
   const editingContent = (
     <div className={styles.text} key={`${contentId}-editing`} ref={editTextRef}>
       <div
+        ref={editingRef}
         contentEditable="plaintext-only"
         suppressContentEditableWarning="true"
       >
@@ -22,8 +24,26 @@ export function LongCardSide({ text, image, isEditing, editTextRef }) {
     </div>
   );
 
+  const handleContentClick = () => {
+    if (!isEditing) return;
+
+    const input = editingRef.current?.lastChild;
+    if (input) {
+      const range = document.createRange();
+      const sel = window.getSelection();
+
+      range.setStart(input, input.length);
+      range.collapse(true);
+
+      sel.removeAllRanges();
+      sel.addRange(range);
+    } else {
+      editingRef.current.focus();
+    }
+  };
+
   return (
-    <div className={styles.content}>
+    <div className={styles.content} onClick={handleContentClick}>
       {image && <img src={image}></img>}
       {isEditing ? editingContent : staticContent}
     </div>
