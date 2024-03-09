beb134a23a5e1d2ef4baf790113887c413afb415
Squikle <mmishaa1702@gmail.com>
2024-03-09 23:24:16 +0100

    added go back button on the sets page

--- src/pages/TermsSetsPage.jsx
+++ src/pages/TermsSetsPage.jsx
@@ -1,12 +1,18 @@
-import "./pageStyles.css";
+import styles from "./TermsSetsPage.module.css";
 import { TermsSet } from "../components/TermsSet/TermsSet";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import GoBackButton from "../components/Buttons/GoBackButton";
+import classname from "classname";
 
 export function TermsSetsPage() {
   const { termsSetId } = useParams();
+  const navigate = useNavigate();
 
   return (
     <main className="container">
+      <div className={classname(styles.backButton, styles.rollout)}>
+        <GoBackButton size="4em" onClick={() => navigate(-1)}></GoBackButton>
+      </div>
       <div className="content">
         <TermsSet termsSetId={termsSetId}></TermsSet>
       </div>
