46719c21327b6445df6391080ba97ba18caf1df9
Squikle <mmishaa1702@gmail.com>
2024-03-10 19:46:52 +0100

    few issues with buttons rework fixed

--- src/components/Buttons/EditSaveButton/EditSaveButton.jsx
+++ src/components/Buttons/EditSaveButton/EditSaveButton.jsx
@@ -2,25 +2,29 @@ import PropTypes from "prop-types";
 import BaseButton from "../BaseButton";
 import { FloppyDiskIcon } from "../../Icons/FloppyDiskIcon";
 import { PencilIcon } from "../../Icons/PencilIcon";
+import classname from "classname";
+import styles from "../Buttons.module.css";
 
-function EditSaveButton({ size, onClick, isEditing }) {
-  const icon = isEditing ? FloppyDiskIcon : PencilIcon;
-  const iconStyle = isEditing ? { margin: "1px 0 0 0" } : {};
-  return (
-    <BaseButton
-      size={size}
-      onClick={onClick}
-      Icon={icon}
-      iconStyle={iconStyle}
-    ></BaseButton>
+function EditSaveButton({ size, onClick, iconClassName, isEditing }) {
+  const icon = isEditing ? (
+    <FloppyDiskIcon
+      style={{ margin: "1px 0 0 0" }}
+      className={classname(iconClassName, styles.defaultIcon)}
+    />
+  ) : (
+    <PencilIcon className={classname(iconClassName, styles.defaultIcon)} />
   );
+  console.log("icon", icon);
+  console.log("size", size);
+
+  return <BaseButton size={size} onClick={onClick} icon={icon}></BaseButton>;
 }
 
 EditSaveButton.propTypes = {
   size: PropTypes.string,
   onClick: PropTypes.func,
-  style: PropTypes.object,
   isEditing: PropTypes.bool,
+  iconClassName: PropTypes.string,
 };
 
 export default EditSaveButton;
